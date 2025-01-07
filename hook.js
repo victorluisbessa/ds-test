64f1d3ae35188641325c6f88594ac0a2103da5ef
victorluisbessa <57152816+victorluisbessa@users.noreply.github.com>
2025-01-07 11:10:41 -0300

    Update hook.js
    changed terminal output to english

--- hook.js
+++ hook.js
@@ -1,17 +1,9 @@
 Java.perform(function() {
-    // Localiza a classe TextView
     var TextView = Java.use('android.widget.TextView');
-
-    // Substitui a implementação do setText com CharSequence
     TextView.setText.overload('java.lang.CharSequence').implementation = function(text) {
-        console.log("Texto original: " + text);
-
-        // Cria o novo texto que será passado para o setText
+        console.log("old text: " + text);
         var newText = Java.use("java.lang.String").$new("This text is currently being hooked.");
-
-        console.log('Novo texto: ' + newText);
-
-        // Chama o método setText com o novo texto
+        console.log('new text: ' + newText);
         return this.setText(newText);
     };
 });
